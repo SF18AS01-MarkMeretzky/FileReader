08c2aaeac14288f99ec33f7519382357373b31ed
Mark Meretzky <mark.meretzky@gmail.com>
2019-01-07 23:13:30 -0500

    Initial Commit

---
+++ FileReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FileReader
+//
+//  Created by Mark Meretzky on 1/7/19.
+//  Copyright © 2019 New York University School of Professional Studies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
